931f37426387a0cec492fcf1ec6593fab65f0945
Yash Mahajan <ymahajan1612@gmail.com>
2025-03-19 14:16:25 +0000

    attempting to use pooling for bert output

--- siamese_bert.py
+++ siamese_bert.py
@@ -10,14 +10,15 @@ class SiameseBert(nn.Module):
         super(SiameseBert, self).__init__()
         self.bert = AutoModel.from_pretrained('bert-base-uncased')
         self.fc = nn.Linear(self.bert.config.hidden_size * 2, 2)
+        self.dropout = nn.Dropout(0.3)
     
     def forward(self, claim_ids, evidence_ids, claim_attention_mask, evidence_attention_mask):
-        claim_output = self.bert(input_ids = claim_ids, attention_mask = claim_attention_mask).last_hidden_state[:,0,:]
-        evidence_output = self.bert(input_ids = evidence_ids, attention_mask = evidence_attention_mask).last_hidden_state[:,0,:]
+        claim_output = self.bert(input_ids = claim_ids, attention_mask = claim_attention_mask).pooler_output
+        evidence_output = self.bert(input_ids = evidence_ids, attention_mask = evidence_attention_mask).pooler_output
 
         # Concatenate the output of the two branches
         combined = torch.cat((claim_output, evidence_output), dim=1)
-        logits = self.fc(combined)
+        logits = self.fc(self.dropout(combined))
         return logits
 
    
